5f43df9db3bc9118b537e268819d6464a05d202a
heiwa4126 <heiwa4126@gmail.com>
2024-09-10 16:26:58 +0900

    initial

---
+++ test/hello-cdk-javascript.test.js
@@ -0,0 +1,16 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const HelloCdkJavascript = require('../lib/hello-cdk-javascript-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hello-cdk-javascript-stack.js
+test("SQS Queue Created", () => {
+	//   const app = new cdk.App();
+	//   // WHEN
+	//   const stack = new HelloCdkJavascript.HelloCdkJavascriptStack(app, 'MyTestStack');
+	//   // THEN
+	//   const template = Template.fromStack(stack);
+	//   template.hasResourceProperties('AWS::SQS::Queue', {
+	//     VisibilityTimeout: 300
+	//   });
+});
